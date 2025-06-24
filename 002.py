464438aaa1a178e855a140e1e1ac854799bbaf5a
Erbolat Usser <e21032003@gmail.com>
2025-06-24 19:41:38 +0500

    Проект 2: Калькулятор

---
+++ 002.py
@@ -0,0 +1,19 @@
+num1=float(input())
+num2=float(input())
+func=input()
+if func=='+':
+    print(num1, '+', num2, '=', num1+num2)
+elif func=='-':
+    print(num1, '-', num2, '=', num1-num2)
+elif func=='*':
+    print(num1, '*', num2, '=', num1*num2)      
+elif func=='/':
+    print(num1, '/', num2, '=', num1/num2)
+elif func=='^':
+    print(num1, '^', num2, '=', num1**num2)      
+elif func=='//':
+    print(num1, '//', num2, '=', num1//num2)    
+elif func=='%':
+    print(num1, '%', num2, '=', num1%num2)    
+else:
+    print('Ошибка!')   
